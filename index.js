975325cfc19b2b8f01a0fa89010479e12f135532
Mohammed Ameen <jegroamin@gmail.com>
2019-12-16 12:25:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
 // Code your solution in this file!
-const companyName = 'Scuber';
-let mostProfitableNeighborhood = "Chelsea";
-let companyCeo ="Susan Smith";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const companyName = 'Scuber';
+// let mostProfitableNeighborhood = "Chelsea";
+// let companyCeo ="Susan Smith";
